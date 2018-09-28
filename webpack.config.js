ec210c3a58b1af33fe38bfdfd05722bc4dee006d
Nathan Ong <nathan.d.ong@gmail.com>
2018-09-27 20:37:13 -0700

    Set up basic dev environment

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const CLI_DIR = path.join(__dirname, './client');
+const DIST_DIR = path.join(__dirname, './client/dist');
+
+module.exports = {
+  entry: `${CLI_DIR}/src/App.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/, // the $ matches the preceding item zero or one time, so this regex also searches for js files too
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: `${CLI_DIR}/index.html`,
+      filename: `${DIST_DIR}/index.html`,
+    }),
+  
+  ],
+};
