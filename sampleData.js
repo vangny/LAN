0f6f97e62c4feb5dcf8f72005e91a9659ff8c6e5
Nathan Ong <nathan.d.ong@gmail.com>
2018-09-28 12:11:02 -0700

    Add sample data

---
+++ sampleData.js
@@ -0,0 +1,32 @@
+const sampleData = [
+    {
+        userID: "12345",
+        createdAt: "9/28/2018, 12:40:52PM",
+        coordinates: [37.705923, -122.134751],
+        event_ID: "12345",
+        photoURL: "https://cdn.vox-cdn.com/thumbor/C1jVKYhTM2-ERlEqor2W-5BbW3A=/0x0:3000x2000/1200x0/filters:focal(0x0:3000x2000):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/9135679/840248942.jpg",
+    },
+    {
+        userID: "54321",
+        createdAt: "9/28/2018 2:00:32PM",
+        coordinates: [37.707146, -122.139128],
+        event_ID: "12345",
+        photoURL: '',
+
+    },
+    {
+        userID: "00000",
+        createdAt: "9/28/2018 2:32:15",
+        coordinates: [37.715701, -122.136036],
+        event_ID: "12345",
+        photoURL: '',
+    },
+    {
+        userID: "10302",
+        createdAt: "9/28/2018 2:45:15",
+        coordinates: [38.349752, -122.709750],
+        event_ID: "",
+        photoURL: '',
+    }
+
+]
