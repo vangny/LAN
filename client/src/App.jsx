d605b0dac5af7a48b13e3e6b5cd4030be53e5113
Nicolas Turner <nicolasrturner@gmail.com>
2018-09-29 11:37:45 -0700

    Refactored App component to fix linter errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { navigate, Link, Router } from '@reach/router';
+import { Link, Router } from '@reach/router';
 import Dashboard from './components/Dashboard.jsx';
 import Alert from './components/Alert.jsx';
 import Home from './Home.jsx';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-    }
 
-    render() {
-        return (
-            <div className="header">
-                <Link to="/">
-                <h1 className="title">Local Alert Network </h1>
-                </Link>
-                <Router>
-                <Home exact path="/" />
-                <Dashboard path="/dashboard" />
-                <Alert path="/alert" />
-                </Router>
-            </div> 
-        );
-    }
-}
+const App = () => (
+  <div className="header">
+    <Link to="/">
+      <h1 className="title">Local Alert Network </h1>
+    </Link>
+    <Router>
+      <Home exact path="/" />
+      <Dashboard path="/dashboard" />
+      <Alert path="/alert" />
+    </Router>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
