6c479d5ee0b4d797eff44183a41baaaacd285371
Nicolas Turner <nicolasrturner@gmail.com>
2018-09-29 13:59:39 -0700

    Added grid layout for Alert component

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 class Alert extends React.Component {
-    render () {
-        return (
-            <div>
-                <h1>RED ALERT</h1>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: '',
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="head">
+        </div>
+        <div className="location-info">
+          <h1>Location Data Here</h1>
+          <h1>Type of Emergency Here</h1>
+        </div>
+        <div className="photo">
+          <button onClick={() => navigate('/')}>Capture Photo</button>
+        </div>
+        <div className="notes">
+          <button onClick={() => navigate('/')}>Notes</button>
+        </div>
+        <div className="submit">
+          <button onClick={() => navigate('/')}>Submit</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Alert;
