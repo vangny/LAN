70c78d00c0db8c4d5a8c5bf9da1346ec428c970c
Nicolas Turner <nicolasrturner@gmail.com>
2018-10-01 14:39:24 -0700

    Added photo title input for the planned full-text image seach capability

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -6,6 +6,8 @@ class Alert extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      photo: [],
+      photoTag: '',
       notes: '',
     };
     this.handleChange = this.handleChange.bind(this);
@@ -18,32 +20,29 @@ class Alert extends Component {
     });
   }
 
+  takePhoto() {
+    this.setState({
+      photo: 'http://www.publicadjustersassociates.com/images/tornado-damages.jpg',
+    });
+  }
+
+  fileHandler(e) {
+    this.setState({
+      photo: e.target.files[0],
+    });
+  }
+
   handleSubmit() {
-    /* Insert emergency event type here */
-    /* Insert geolocation data here */
-    const { notes } = this.state;
-    const alertData = { notes };
-
-    // const query = {
-    //   "query": "query aTest($arg1: String!) { test(who: $arg1) }",
-    //   "operationName": "aTest",
-    //   "variables": { "arg1": "me" }
-    // }
-    // fetch('/graphql', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Accept': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     query,
-    //     variables: { alertData }
-    //   })
-    //   })
-    //   .then(r => r.json())
-    //   .then(data => console.log('data returned:', data));
-    // }
-
+    const category = 'Flood';
+    const geolocation = [37.786831, -122.410488];
+    const { notes, photo, photoTag } = this.state;
+    const alertData = {
+      category,
+      geolocation,
+      notes,
+      photo,
+      photoTag,
+    };
     axios.post('/alert/api/alerts', alertData)
       .then((res) => {
         console.log('alert sent to server', alertData);
@@ -58,17 +57,18 @@ class Alert extends Component {
     console.log(`category: ${category}\nlatitude: ${latitude}\nlongitude: ${longitude}\nEventID: ${EventID}`);
     return (
       <div className="container">
-        <div className="head">
-        </div>
+        <div className="head" />
         <div className="location-info">
           <h1>Location Data Here</h1>
           <h1>Type of Emergency Here</h1>
         </div>
         <div className="photo">
-          <button onClick={() => navigate('/')}>Capture Photo</button>
+          <button type="button" onClick={this.takePhoto}>Capture Photo</button>
+          <input type="text" name="photoTag" placeholder="Describe your photo" onChange={this.handleChange} value={photoTag} />
+          <input type="file" onChange={this.fileHandler} />
         </div>
         <div className="notes">
-          <input type="text" name="notes" placeholder="Enter text here" onChange={this.handleChange} value={notes} />
+          <input size="" type="text" name="notes" placeholder="Enter text here" onChange={this.handleChange} value={notes} />
         </div>
         <div className="submit">
           <button type="button" onClick={this.handleSubmit}>Submit</button>
---
+++ client/src/components/alert-components/TakePhoto.jsx
@@ -0,0 +1,27 @@
+// import React, { Component } from 'react';
+// import Camera from 'react-html5-camera-photo';
+// import 'react-html5-camera-photo/build/css/index.css';
+
+// class TakePhoto extends Component {
+//     constructor(props) {
+//         super(props);
+//     this.onTakePhoto = this.onTakePhoto.bind(this);
+//     }
+
+//   onTakePhoto(dataUri) {
+//         console.log('take photo');
+//     }
+
+
+// render() {
+//     return (
+//       <div classNam="App">
+//         <Camera
+//             onTakePhoto={(dataUri) => { this.onTakePhoto(dataUri); }}
+//           />
+//         </div>
+//     );
+//   }
+// }
+
+// export default TakePhoto;
