cd7324c4d07dae76e95f1440d0eef1dafdf70c16
Nathan Ong <nathan.d.ong@gmail.com>
2018-10-01 21:03:14 -0700

    Refactor AlertOptions and App to reduce redundancy.
    No need for AlertOptions to be stateful component

--- client/src/components/AlertOptions.jsx
+++ client/src/components/AlertOptions.jsx
@@ -1,67 +1,24 @@
-import React, { Component } from 'react';
-import { Link, Router, navigate } from '@reach/router';
-import moment from 'moment';
-import Alert from './Alert';
+import React from 'react';
 
-class AlertOptions extends React.Component {
-  constructor(props) {
-    super(props);
-    const { latitude, longitude } = this.props;
-    this.state = {
-      latitude,
-      longitude,
-      category: null,
-      timeStamp: moment().format(),
-    };
-    this.componentDidMount = this.componentDidMount.bind(this);
-    this.sendDataToApp = this.sendDataToApp.bind(this);
-    // this.clickHandler = this.clickHandler.bind(this);
-  }
 
-  componentDidMount() {
-    navigator.geolocation.watchPosition((position) => {
-      this.setState({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      });
-      console.log(position.coords.latitude, position.coords.longitude);
+const AlertOptions = ({ latitude, longitude, appContext, handleAlertOptions }) => {
+
+  navigator.geolocation.watchPosition((position) => {
+    appContext.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    }, () => {
+      console.log('Adjusted coordinates: ', position.coords.latitude, position.coords.longitude);
     });
-  }
-
-  sendDataToApp(category) {
-    const { appHandler } = this.props;
-    const { latitude, longitude, timeStamp } = this.state;
-    appHandler(category, latitude, longitude, timeStamp);
-  }
-
-  // clickHandler(category) {
-  //   console.log(category);
-  //   this.setState({ category });
-  //   setTimeout(() => {
-  //     navigate('/alert')
-  //   }, 1000)
-  //   navigate('alert');
-  // };
-
-  render() {
-    const { latitude, longitude, category } = this.state;
-    // console.log(this.state);
-    const test = 'test';
-    return (
-      <div>
-        <div>{`lat: ${latitude} long: ${longitude}`}</div>
-        <Link to="alert" type="button">LINK TEST</Link>
-        <button type="button" onClick={this.clickHandler}>test</button>
-        <button id="hurricane" type="button" value="hurricane" onClick={e => navigate('alert', this.sendDataToApp(e.target.value))}>hurricane</button>
-        <button id="flood" type="button" value="flood" onClick={e => navigate('alert', this.sendDataToApp(e.target.value))}>flood</button>
-        <button id="wildfire" type="button" value="wildfire" onClick={e => navigate('alert', this.sendDataToApp(e.target.value))}>wildfire</button>
-        <button id="earthquake" type="button" value="earthquake" onClick={e => navigate('alert', this.sendDataToApp(e.target.value))}>earthquake</button>
-        <Router>
-          <Alert path="/alert" latitude={latitude} longitude={longitude} category={category} test={test} />
-        </Router>
-      </div>
-
-    );
-  }
-}
+  });
+  return (
+    <div>
+      <div>{`lat: ${latitude} long: ${longitude}`}</div>
+      <button id="hurricane" type="button" value="hurricane" onClick={e => handleAlertOptions(e.target.value)}>hurricane</button>
+      <button id="flood" type="button" value="flood" onClick={e => handleAlertOptions(e.target.value)}>flood</button>
+      <button id="wildfire" type="button" value="wildfire" onClick={e => handleAlertOptions(e.target.value)}>wildfire</button>
+      <button id="earthquake" type="button" value="earthquake" onClick={e => handleAlertOptions(e.target.value)}>earthquake</button>
+    </div>
+  );
+};
 export default AlertOptions;
