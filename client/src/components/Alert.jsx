4230ba19028288053febe2b5fa7cf655cd42291f
Nicolas Turner <nicolasrturner@gmail.com>
2018-10-02 23:10:46 -0700

    Added method to upload photos to Cloudinary for storage- API request successful

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Dropzone from 'react-dropzone';
 import { navigate } from '@reach/router';
 import AlertCamera from './alert-components/Camera.jsx';
 import Modal from './alert-components/modal.jsx';
-
 
 class Alert extends Component {
   constructor(props) {
@@ -17,6 +17,7 @@ class Alert extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.waitForData = this.waitForData.bind(this);
+    // this.handleDrop = this.handleDrop.bind(this);
   }
 
   handleChange(e) {
@@ -72,6 +73,34 @@ class Alert extends Component {
       </p>
     );
   }
+  /* eslint-disable */
+  handleDrop(files) {
+    const upload = files.map((file) => {
+      let formData = new FormData();
+      formData.append('file', file);
+      formData.append('tags', 'test');
+      formData.append('upload_preset', 'pnqbmmuw'); // cloud name
+      formData.append('api_key', '491544247472459') // api key
+      formData.append('timestamp', (Date.now() / 1000) | 0);
+          // Cloudinary upload request
+      
+      return axios.post('https://api.cloudinary.com/v1_1/n3/image/upload', formData, {
+        headers: { "X-Requested-With": "XMLHttpRequest" },
+        
+      }).then((res) => {
+        console.log('trying to send this:', formData)
+        const data = res.data;
+        const fileURL = data.secure_url
+        console.log(data);
+      })
+    });
+
+      // After all files uploaded
+      axios.all(upload).then(() => {
+          console.log('All photos successfully uploaded');
+      });
+  }
+  /* eslint-enable */
 
   changeModal(view) {
     this.setState({
@@ -117,6 +146,16 @@ class Alert extends Component {
         <div className="notes">
           <input size="" type="text" name="notes" placeholder="Enter text here" onChange={this.handleChange} value={notes} />
         </div>
+        <Dropzone
+          className="dropzone"
+          name="file"
+          type="file"
+          onDrop={this.handleDrop}
+          multiple
+          accept="image/*"
+        >
+          <p>Drop files or click here to upload</p>
+        </Dropzone>
         <div className="submit">
           {this.waitForData()}
         </div>
