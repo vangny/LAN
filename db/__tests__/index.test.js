63dbb63eb93684f482af4d5d6b110f7e2233b5d1
Nathan Ong <nathan.d.ong@gmail.com>
2018-10-03 19:42:35 -0700

    Add basic test to db to pass test suite

--- db/__tests__/index.test.js
+++ db/__tests__/index.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const sum = require('../../server/sum');
 
 // test
@@ -5,3 +6,11 @@ describe('Test sum function', () => {
   test('It should properly sum two numbers', () => (
     expect(sum(1, 2)).toBe(3)));
 });
+=======
+const sum = require('../../server/sum')
+
+describe('Test sum function', () => {
+  test('It should properly sum two numbers', () => (
+    expect(sum(1, 2)).toBe(3)));
+})
+>>>>>>> Add basic test to db to pass test suite
