254d7638e746572cd32025f43dadadd3a56ab769
vangny <vang.nathany@gmail.com>
2018-10-08 12:19:57 -0500

    Add image in LoadingPage

--- client/src/components/LoadingPage.jsx
+++ client/src/components/LoadingPage.jsx
@@ -3,7 +3,7 @@ import 'React' from 'react';
 const LoadingPage = () => {
   return (
     <div>
-      <div className="loadingIMG"></div>
+      <div className="loadingIMG"><img src="../../../icons/icon-512x512.png"/></div>
       <span>L</span><span>ocal </span>
       <span>A</span><span>lert </span>
       <span>N</span><span>etwork</span>
